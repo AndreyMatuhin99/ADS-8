4514500e200bb50a2b7fcc7d9b225fde4b8ceaaf
AndreyMatuhin99 <tronder.yt@gmail.com>
2024-05-12 11:56:08 +0300

    Задание 8

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,42 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() {
+    first = nullptr;
+    countOp = 0;
+}
+
+void Train::addCage(bool light) {
+    Cage *newCage = new Cage;
+    newCage->light = light;
+
+    if (!first) {
+        first = newCage;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Cage *lastCage = first->prev;
+        lastCage->next = newCage;
+        newCage->prev = lastCage;
+        newCage->next = first;
+        first->prev = newCage;
+    }
+}
+
+int Train::getLength() {
+    if (!first)
+        return 0;
+
+    int length = 0;
+    Cage *current = first;
+    do {
+        length++;
+        current = current->next;
+    } while (current != first);
+
+    return length;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
